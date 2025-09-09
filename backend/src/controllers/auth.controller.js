75cb778b0e2bf96d853c3ec21e9300f0f750336f
Jatin <jatinvaid00540@gmail.com>
2025-09-09 21:29:23 +0530

    Initial commit: project setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export async function signup(req,res){
+    res.send("Signup Route");
+}
+
+export async function login(req,res){
+    res.send("login Route");
+}
+
+export function logout(req,res){
+    res.send("Logout Route");
+}
