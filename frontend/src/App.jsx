8ae7973d5f8a939754689c1cd44334b5f6ebf414
Jatin <jatinvaid00540@gmail.com>
2025-09-24 00:33:46 +0530

    Video Call page Working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -65,7 +65,16 @@ const App = () => {
             )
           }
         />
-      <Route path="/call" element={isAuthenticated ? <CallPage />: <Navigate to="/login"/>} />
+      <Route
+          path="/call/:id"
+          element={
+            isAuthenticated && isOnboarded ? (
+              <CallPage />
+            ) : (
+              <Navigate to={!isAuthenticated ? "/login" : "/onboarding"} />
+            )
+          }
+        />
       <Route
           path="/chat/:id"
           element={
@@ -78,7 +87,7 @@ const App = () => {
             )
           }
         />
-        
+
       <Route
           path="/onboarding"
           element={
